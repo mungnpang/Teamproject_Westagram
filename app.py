42babe7c352695fafff1f97b29630a886417dc8f
mungnpang <koressma@gmail.com>
2021-12-31 16:09:13 +0900

    추가- mypage html/css

--- app.py
+++ app.py
@@ -35,21 +35,21 @@ def email_check(email):
     return bool(db.users.find_one({'email': email}))
 
 
-@app.route('/mypage', methods=['GET','POST'])
-def profile_img():
-    valid = valid_token()
-    if type(valid) == dict:
-        if request.method == 'GET':
-            return render_template('mypage.html')
-        else:
-            file = request.files['file']
-            img_name = uuid4().hex
-            file.save('./static/media/profile_img/' + img_name)
-            img_id = '../static/media/profile_img/' + img_name
-            db.users.update_one({'email':valid['id']}, {'$profile_img':img_id})
-            return jsonify({'result':'success'})
-    else:
-        return redirect(url_for('login'))
+# @app.route('/mypage', methods=['GET','POST'])
+# def profile_img():
+#     valid = valid_token()
+#     if type(valid) == dict:
+#         if request.method == 'GET':
+#             return render_template('mypage.html')
+#         else:
+#             file = request.files['file']
+#             img_name = uuid4().hex
+#             file.save('./static/media/profile_img/' + img_name)
+#             img_id = '../static/media/profile_img/' + img_name
+#             db.users.update_one({'email':valid['id']}, {'$profile_img':img_id})
+#             return jsonify({'result':'success'})
+#     else:
+#         return redirect(url_for('login'))
 
 
 @app.route('/')
@@ -57,6 +57,7 @@ def main():
     valid = valid_token()
     if type(valid) == dict:
         feeds = list(db.feeds.find({}, {'_id':False, 'email':False}))
+        feeds.reverse()
         return render_template('index.html', feeds=feeds)
     else:
         return redirect(url_for('login'))
@@ -96,19 +97,19 @@ def join():
         #이메일 유효성 검사
         if (re.search('[^a-zA-Z0-9-_.@]+', email) is not None
                 or not (9 < len(email) < 26)):
-            return jsonify({'result': 'failed', 'msg': '휴대폰번호 또는 이메일의 형식을 확인해주세요. 영문과, 숫자, 일부 특수문자(.-_) 사용 가능. 10~25자 길이'})
+            return jsonify({'result': 'error', 'msg': '휴대폰번호 또는 이메일의 형식을 확인해주세요. 영문과, 숫자, 일부 특수문자(.-_) 사용 가능. 10~25자 길이'})
         # 비밀번호 유효성 검사
         elif (re.search('[^a-zA-Z0-9!@#$%^&*]+', temp_password) is not None or
               not (7 < len(temp_password) < 21) or
               re.search('[0-9]+', temp_password) is None or
               re.search('[a-zA-Z]+', temp_password) is None):
-            return jsonify({'result': 'failed', 'msg': '비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 일부 특수문자(!@#$%^&*) 사용 가능. 8~20자 길이'})
+            return jsonify({'result': 'error', 'msg': '비밀번호의 형식을 확인해주세요. 영문과 숫자 필수 포함, 일부 특수문자(!@#$%^&*) 사용 가능. 8~20자 길이'})
         # 빈칸 검사
         elif not (email and name and nickname and temp_password):
-            return jsonify({'result': 'failed', 'msg': '빈칸을 입력해주세요.'})
+            return jsonify({'result': 'error', 'msg': '빈칸을 입력해주세요.'})
         # 중복 이메일 검사
         elif email_check(email):
-            return jsonify({'result': 'failed', 'msg': '가입된 내역이 있습니다.'})
+            return jsonify({'result': 'error', 'msg': '가입된 내역이 있습니다.'})
         doc = {'email':email, 'name':name, 'nickname':nickname, 'password':password}
 
         db.users.insert_one(doc)
